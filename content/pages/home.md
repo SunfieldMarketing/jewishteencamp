d3247124dc86da094ff7327a389bfadb073be0ef
Kauan Da Silva <kauan@sunfieldmarketing.com>
2026-04-19 04:47:30 -0400

    admin

---
+++ content/pages/home.md
@@ -0,0 +1,5 @@
+---
+title: Chicago Jewish Teens Camp
+---
+
+Welcome to the Chicago Jewish Teens Camp! This text is now editable via your visual dashboard.
---
+++ tina/config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  branch: process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main",
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || null, 
+  token: process.env.TINA_TOKEN || null, 
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "page",
+        label: "Pages",
+        path: "content/pages",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Main Content",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
