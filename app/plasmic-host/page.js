36b1f0b7d870dc23b5768a2ae3c5a39acc7edcb2
Kauan Da Silva <kauan@sunfieldmarketing.com>
2026-04-23 13:51:44 -0400

    Host Fix: Activated component registration on plasmic-host route

--- app/plasmic-host/page.js
+++ app/plasmic-host/page.js
@@ -1,8 +1,20 @@
-﻿'use client';
+'use client';
 import * as React from 'react';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
-import { PLASMIC } from '../../plasmic-init';
+import { PLASMIC, registerComponents } from '../../plasmic-init';
 
+/**
+ * PLASMIC STUDIO HOST
+ * This is the doorway the visual editor uses to talk to your code.
+ */
 export default function HostPage() {
-  return PLASMIC ? <PlasmicCanvasHost /> : <div>Initializing Bridge...</div>;
+  React.useEffect(() => {
+    // We must register components for the Studio to see them!
+    const init = async () => {
+      await registerComponents();
+    };
+    init();
+  }, []);
+
+  return PLASMIC && <PlasmicCanvasHost />;
 }
