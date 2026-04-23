9cc63e0a101169573bf119fc3d44a98fb40ebcaf
Kauan Da Silva <kauan@sunfieldmarketing.com>
2026-04-23 04:42:41 -0400

    Fix nested folder issue and update catch-all router

---
+++ app/[[...segments]]/page.js
@@ -0,0 +1,30 @@
+﻿import {
+  PlasmicComponent,
+  PlasmicRootProvider,
+} from "@plasmicapp/loader-nextjs";
+import { notFound } from "next/navigation";
+import { PLASMIC } from "../../plasmic-init";
+
+export default async function PlasmicPage({ params }) {
+  const segments = params.segments;
+  const plasmicPath = segments ? `/${segments.join("/")}` : "/";
+  
+  const plasmicData = await PLASMIC.maybeFetchComponentData(plasmicPath);
+  if (!plasmicData) {
+    notFound();
+  }
+
+  const pageMeta = plasmicData.entryCompMetas[0];
+  return (
+    <PlasmicRootProvider loader={PLASMIC} prefetchedData={plasmicData}>
+      <PlasmicComponent component={pageMeta.displayName} />
+    </PlasmicRootProvider>
+  );
+}
+
+export async function generateStaticParams() {
+  const pages = await PLASMIC.fetchPages();
+  return pages.map((page) => ({
+    segments: page.path.substring(1).split("/"),
+  }));
+}
